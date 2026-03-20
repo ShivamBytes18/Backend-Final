d10d2c0f72a2e34e8c270aacc15758ab92d234af
ShivamBytes18 <su9555181178@gmail.com>
2026-03-20 22:18:14 +0530

    Data Add on

---
+++ src/utils/asynchandler.js
@@ -0,0 +1,34 @@
+const asynchandler = (requestHandler) =>{
+    (req,res,next)=>
+    {
+        Promise.resolve(requestHandler(req,res)).
+        catch((err) => next(err))
+    }
+}
+export {asynchandler}
+
+
+
+
+
+
+
+
+
+
+
+// const asynchandler = ()=>{}
+// const asynchandler = (func) =>{() => {}}
+
+// const asynchandler = (fn)=> async (req,res,next)=>{
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(error.code || 500).json(
+//             {
+//                 success : false,
+//                 message : error.message
+//             }
+//         )
+//     }
+// }

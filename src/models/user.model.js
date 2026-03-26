8bbd2578f3f8dfd3043a56dfd481d80bda8039f4
ShivamBytes18 <su9555181178@gmail.com>
2026-03-27 03:26:10 +0530

    Add on new

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -56,10 +56,10 @@ userSchema.pre("save",async function (next){
 })
 userSchema.methods.isPasswordCorrect= async function 
 (password){
-    bcrypt.compare(password,this.password)
+   return await bcrypt.compare(password,this.password)
 }
 userSchema.methods.generateAccessToken= function (){
-    jwt.sign({
+   return jwt.sign({
         _id:this._id,
         email:this.email,
         username:this.username,

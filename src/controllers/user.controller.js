216896565f57f170ada469403644061f7ade0055
ShivamBytes18 <su9555181178@gmail.com>
2026-03-30 02:54:03 +0530

    Add on new

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,22 +1,24 @@
 import {asynchandler} from "../utils/asynchandler.js"
 import {ApiError} from "../utils/ApiError.js"
 import { User } from "../models/user.model.js"
-
+import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { response } from "express"
 
 const registerUser = asynchandler(async (req,res)=>{
 
 const {fullName,email,username,password}=req.body
-console.log("fullName",fullName);
-console.log("Email",email);
-console.log("Password",password);
+// console.log("fullName",fullName);
+// console.log("Email",email);
+// console.log("Password",password);
 
-if(
+if(   
     [fullName,email,username,password].some((field)=>
     field?.trim() === "")
 ){
     throw new ApiError(400,"All Fields are required")
 }
-   const existedUser = User.findOne({
+   const existedUser = await User.findOne({
  $or:[{username},{email}]
 })
 
@@ -25,6 +27,44 @@ if(existedUser){
 }
   const avatarLocalPath = req.files?.avatar[0]?.path;
   const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  // let coverImageLocalPath
+ /* if(req.files && Array.
+    isArray(req.files.coverImage) && req.
+    files.coverImage.length > 0){
+        coverImageLocalPath = req.files.coverImage[0].path
+    }*/ 
+  //console.log(req.files)
+if(! avatarLocalPath)
+{
+    throw new ApiError(400,"Avatar file is required")
+}
+ 
+ const avatar = await uploadOnCloudinary(avatarLocalPath)
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+  if (!avatar){
+
+     throw new ApiError(400,"Avatar file is required")
+  }
+ const user =await  User.create({
+    fullName,
+    avatar :avatar.url,
+    coverImage:coverImage?.url || "",
+    email,
+    password,
+    username:username.toLowerCase()
+  })
+  const createdUser= await User.findById(user._id).select(
+    "-password -refreshToken"
+  )
+if(!createdUser)
+{
+    throw new ApiError(505,"Something went Wrong while Regestering the User")
+}
+
+return res.status(201).json(
+    new ApiResponse(200,createdUser,"User registred Successfully")
+)
 })
 
 export {registerUser}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -11,7 +11,7 @@ router.route("/register").post(
           maxCount:1  
         },
         {
-          name:"coverIamge",
+          name:"coverImage",
           maxCount:1  
         }
     ])
